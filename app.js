6807f3fb9c6c8a3340b25dbce109904057fb1164
Alex Votry <alexvotry@gmail.com>
2016-04-06 15:31:09 -0700

    installed session key

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.suer(cookieSession({
+app.use(cookieSession({
   name: 'session',
   keys: [process.env.SESSION_KEY]
 }));
